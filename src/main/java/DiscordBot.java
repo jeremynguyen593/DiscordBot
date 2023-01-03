62e54fd744984e0a10d9f2054c9c18685e1c7502
jeremynguyen593 <jeremynugyen593@gmail.com>
2023-01-02 22:32:01 -0800

    Created the discord bot and a way to retrieve users' inputs.

---
+++ src/main/java/DiscordBot.java
@@ -0,0 +1,25 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+
+public class DiscordBot extends ListenerAdapter{
+    public static void main(String[] args) throws LoginException {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        final String DISCORD = dotenv.get("DISCORD_API_KEY");
+
+        JDABuilder jdaBuilder = JDABuilder.createDefault(DISCORD);
+        jdaBuilder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
+        JDA jda = jdaBuilder.build();
+
+        jda.addEventListener(new CommandManager());
+    }
+
+}
