687dbbe804507e8633d6d83b69987f53187b5be6
jeremynguyen593 <jeremynugyen593@gmail.com>
2023-01-18 23:57:54 -0800

    Fixed the issue that the SkipCommand was not being added to git pushes

---
+++ src/main/java/commands/SkipCommand.java
@@ -0,0 +1,15 @@
+package commands;
+
+import lavaplayer.GuildMusicManager;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+
+public class SkipCommand {
+    public static void skip(GuildMusicManager musicManager, MessageChannel channel) {
+        if(musicManager.scheduler.getPlayingTrack()==null) {
+            channel.sendMessage("Music is not currently playing.").queue();
+        } else {
+            musicManager.scheduler.nextTrack();
+        }
+
+    }
+}
